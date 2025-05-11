80f85ff81fa436f047394c7ff18cd1dc497081ed
Jason Widjaja <118709081+JasonW35214@users.noreply.github.com>
2025-05-10 23:43:39 -0400

    Added provider entries for kvm@tacc data

--- tf/kvm/data.tf
+++ tf/kvm/data.tf
@@ -4,50 +4,62 @@
 # }
 
 data "openstack_networking_network_v2" "sharednet2" {
+  provider = openstack.kvm
   name = "sharednet2"
 }
 
 data "openstack_networking_network_v2" "sharednet1" {
+  provider = openstack.kvm
   name = "sharednet1"
 }
 
 data "openstack_networking_subnet_v2" "sharednet1_subnet" {
+  provider = openstack.kvm
   name = "sharednet1-subnet"
 }
 
 data "openstack_networking_secgroup_v2" "allow_ssh" {
+  provider = openstack.kvm
   name = "allow-ssh"
 }
 
 data "openstack_networking_secgroup_v2" "allow_9001" {
+  provider = openstack.kvm
   name = "allow-9001"
 }
 
 data "openstack_networking_secgroup_v2" "allow_8000" {
+  provider = openstack.kvm
   name = "allow-8000"
 }
 
 data "openstack_networking_secgroup_v2" "allow_8080" {
+  provider = openstack.kvm
   name = "allow-8080"
 }
 
 data "openstack_networking_secgroup_v2" "allow_8081" {
+  provider = openstack.kvm
   name = "allow-8081"
 }
 
 data "openstack_networking_secgroup_v2" "allow_http_80" {
+  provider = openstack.kvm
   name = "allow-http-80"
 }
 
 data "openstack_networking_secgroup_v2" "allow_9090" {
+  provider = openstack.kvm
   name = "allow-9090"
 }
 
 data "openstack_networking_secgroup_v2" "allow_3000" {
+  provider = openstack.kvm
   name = "allow-3000"
 }
 
 data "openstack_networking_secgroup_v2" "allow_8265" {
+  provider = openstack.kvm
   name = "allow-8265"
 }
 
