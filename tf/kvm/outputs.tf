03033ce923b4b7a53208f88f8899f4d1162d2375
Jason Widjaja <118709081+JasonW35214@users.noreply.github.com>
2025-05-10 23:14:24 -0400

    Update outputs.tf for Chi@Edge

--- tf/kvm/outputs.tf
+++ tf/kvm/outputs.tf
@@ -2,3 +2,8 @@ output "floating_ip_out" {
   description = "Floating IP assigned to node1"
   value       = openstack_networking_floatingip_v2.floating_ip.address
 }
+
+output "edge_floating_ip_out" {
+  description = "Floating IP assigned to CHI@Edge FastAPI node"
+  value       = openstack_networking_floatingip_v2.edge_floating_ip.address
+}
