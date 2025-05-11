c9f8186918a54e89d8c592614860e08b269ccf33
Jaden Antoine <jadenjantoine@gmail.com>
2025-05-11 16:11:35 -0400

    Update app.py

--- fastapi/app.py
+++ fastapi/app.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import numpy as np
 import joblib
+import hashlib
 
 # Initialize FastAPI app
 app = FastAPI(
@@ -15,12 +16,11 @@ model = joblib.load("crash_model.joblib")  # Update path if needed
 
 # Define input data structure
 class IntersectionFeatures(BaseModel):
-    intersection_id: int
+    intersection_id: str
     accidents_6m: int
     accidents_1y: int
     accidents_5y: int
 
-# Health check
 @app.get("/test")
 async def root():
     return {"message": "FastAPI is running"}
@@ -29,11 +29,22 @@ async def root():
 @app.post("/predict")
 def predict(data: IntersectionFeatures):
     input_data = np.array([[
-        data.intersection_id,
+        int(hashlib.sha256(data.intersection_id.encode('utf-8')).hexdigest(), 16),
         data.accidents_6m,
         data.accidents_1y,
         data.accidents_5y
     ]])
-
+    
     prediction = model.predict(input_data)
-    return {"predicted_future_accidents_6m": float(prediction[0])}
+    label = ""
+    if(float(prediction[0]) <= 1):
+        label = "Safe"
+    elif(float(prediction[0]) <= 3):
+        label = "Caution"
+    elif(float(prediction[0]) > 3):
+        label = "Dangerous"
+
+    return {
+        "predicted_future_accidents_6m": float(prediction[0]),
+        "label_classification" : label
+   }
