e1a736f2ef5b6fdfce7ddd6456e7c4722429903e
Jason Widjaja <118709081+JasonW35214@users.noreply.github.com>
2025-05-10 22:08:17 -0400

    added provider entry for Chi@Edge

--- tf/kvm/provider.tf
+++ tf/kvm/provider.tf
@@ -2,17 +2,12 @@ provider "openstack" {
   cloud = "openstack"
 }
 
-/*
-provider "openstack" {
-  alias = "openstack"
-  cloud = "openstack" 
-}
-
 provider "openstack" {
   alias = "chiedge"
   cloud = "chiedge"   
 }
 
+/*
 provider "openstack" {
   alias = "chitacc"
   cloud = "chitacc"   
